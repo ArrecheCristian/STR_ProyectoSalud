8280fa8004ff8e212208195b4ca5791442c0eff4
Martín Paradiso <martinparadiso@protonmail.com>
2019-12-03 00:24:42 -0300

    Actualizado visualizer para cargar mapas. Agregada definición de mapa.

--- README.md
+++ README.md
@@ -14,3 +14,31 @@
 ## Notas
 
 * **IMPORTANTE** Explicar como funcionan las dos probabilidades, como se combinan.
+
+## Formato del mapa
+
+El mapa se almacena con la siguiente estructura:
+
+- La primer linea indica el tamaño del mapa, en formato AnchoxAlto
+- Las coordenadas (0,0) se encuentra en la primer linea del archivo, en el primer caracter
+- Cada linea representa una fila, las columnas se encuentran separadas por un espacio
+- Los *objetos* se almacenan codificadas como números, de la siguiente manera:
+
+| Codigo    | Entidad           |
+| --------- | ----------------- |
+| 0         | Vacío             |
+| 1         | Pared             |
+| 2         | Persona sana      |
+| 3         | Persona enferma   |
+
+A modo de ejemplo, el siguiente texto representa un mapa de 5x5, con una entrada a la izquierda
+y una persona enferma en el centro, y una persona sana a la derecha.
+
+```
+5x5
+1 1 1 1 1
+1 0 0 0 1
+0 0 3 2 1
+1 0 0 0 1
+1 1 1 1 1
+```
--- utils/visualizer.py
+++ utils/visualizer.py
@@ -26,14 +26,42 @@ assets = [
     Image.open("assets/persona_sana.png"),      # 2
     Image.open("assets/persona_enferma.png")    # 3
 ]
-
 assets_size = (32, 32)                                  # Tamaño de los assets
 
-map_size_units = ( 200, 200 )
-map_size_px = ( map_size_units[0] * assets_size[0], map_size_units[1] * assets_size[1])     # Tamaño del mapa TODO ahora está forzado a mano, pero habría que levantarlo del archivo de mapa
+# Carga el mapa
+mapa = [ ]
+
+with open(args.plano, "r") as f:
+    primer_linea = f.readline()
+    ancho = re.match("(?P<ancho>[0-9]+)x(?P<alto>[0-9]+)", primer_linea )["ancho"]
+    alto = re.match("(?P<ancho>[0-9]+)x(?P<alto>[0-9]+)", primer_linea )["alto"]
+    map_size_units = (int(ancho), int(alto))
+
+    y = 0
+    for linea in f:
+        fila = re.findall("[0-9]+[ \n]", linea)
+        mapa.append([])    # Por temas de python hay que inicializar el vector
+
+        for columna in fila:
+            tipo = int(columna)
+
+            # Si no se paso información de la simulación dibuja a las personas,
+            # no tiene sentido hacerlo si se pasan los ticks, ya que esas personas
+            # son solo válidas para el instante incial
+            if not args.datos:
+                mapa[y].append(tipo)
+            
+        y += 1
+        
+
+map_size_px = ( map_size_units[0] * assets_size[0], map_size_units[1] * assets_size[1])
 
 # Genera una imágen de prueba
 imagen = Image.new('RGB', map_size_px)
+
+# TODO si recibe tamibien un archivo de datos, procesar cada
+# tick y en cada tick generar una imágen diferente a partir del
+# mismo fondo+pared
 
 # Si recibe datos, los grafica
 if args.datos:
@@ -45,4 +73,16 @@ if args.datos:
         tipo = int(agente[2])
         imagen.paste( assets[tipo], (x * assets_size[0], y * assets_size[1]) )
 
+# Si no recibio datos, grafica solo el mapa
+else:
+    
+    for y in range(0, map_size_units[1] ):
+        for x in range(0, map_size_units[0] ):
+            print(mapa[y][x], end=" ")
+
+            imagen.paste( assets[ mapa[y][x] ], (x * assets_size[0], y * assets_size[1]) )
+
+        print()
+
+            
 imagen.save("prueba.jpg", "JPEG")
